395529aa6d08fe32223e3b9a3b512688d072c81c
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-04-19 16:17:20 +0530

    Database Migration To PostgreSQL

--- AssistantsWeb/settings.py
+++ AssistantsWeb/settings.py
@@ -20,10 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@dxuib_9*pu+qy!z6o=+z(nyvlwd!%3n(v#an1au&)$8bo$lkx'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
